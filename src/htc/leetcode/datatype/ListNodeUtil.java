9f030e064f072a39525d18697d76b0ad2db3dfa7
hteason <617430866@qq.com>
2020-07-13 22:59:25 +0800

    ListNodeUtil乱码修复

--- src/htc/leetcode/datatype/ListNodeUtil.java
+++ src/htc/leetcode/datatype/ListNodeUtil.java
@@ -6,7 +6,7 @@ public class ListNodeUtil {
 		ListNodeUtil.print(ListNodeUtil.str2Node("1 ->  1->2->3->3"));
 	}
 
-	//����/��������ת����
+	//数组/不定参数转链表
 	public static ListNode arr2Node(int...arr) {
 		ListNode head = new ListNode(-1);
 		ListNode listNode = head;
@@ -17,9 +17,9 @@ public class ListNodeUtil {
 		}
 		return head.next;
 	}
-	
 
-	//ר����leetcode���������ĸ���
+
+	//专用于leetcode测试样例的复制
 	public static ListNode str2Node(String s) {
 		if(s==null||s.trim().length()==0) {
 			return null;
@@ -35,7 +35,7 @@ public class ListNodeUtil {
 		return head.next;
 	}
 
-	//�������
+	//输出链表
 	public static void print(ListNode head) {
 		while (head != null) {
 			System.out.print(head.val + " -> ");
