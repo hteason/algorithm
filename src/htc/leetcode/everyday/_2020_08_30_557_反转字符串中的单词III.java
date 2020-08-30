fcc8911ce7121fd1efc822e6352af3ca82bce323
hteason <617430866@qq.com>
2020-08-30 12:30:03 +0800

    LeetCode[每日一题/字符串] 557.反转字符串中的单词III(easy) AC 2020-8-30 https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/

--- src/htc/leetcode/everyday/_2020_08_30_557_反转字符串中的单词III.java
+++ src/htc/leetcode/everyday/_2020_08_30_557_反转字符串中的单词III.java
@@ -7,7 +7,31 @@ package htc.leetcode.everyday;
 public class _2020_08_30_557_反转字符串中的单词III {
     public static void main(String[] args) {
         _2020_08_30_557_反转字符串中的单词III test = new _2020_08_30_557_反转字符串中的单词III();
-        System.out.println(test.reverseWords2("Let's take LeetCode contest"));
+        System.out.println(test.reverseWords3("Let's take LeetCode contest"));
+    }
+
+    //纯数组，几个中最快的
+    public String reverseWords3(String s) {
+        char[] ss = s.toCharArray();
+        char[] ans = new char[ss.length];
+
+        for (int i = 0, index = 0, len = ss.length; i < len; i++) {
+            int j = i;
+            while (j < len && ss[j] != ' ') {
+                j++;
+            }
+            if (j > len) {
+                break;
+            }
+            for (int k = j - 1; k >= i; k--) {
+                ans[index++] = ss[k];
+            }
+            if (j <= len - 1) {
+                ans[index++] = ' ';
+            }
+            i = j;
+        }
+        return new String(ans);
     }
 
     public String reverseWords2(String s) {
