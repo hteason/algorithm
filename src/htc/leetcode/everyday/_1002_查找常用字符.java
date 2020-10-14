6f44cad3f18db56435d7e187cf2828dd7ec2f941
hteason <617430866@qq.com>
2020-10-14 23:33:36 +0800

    LeetCode[每日一题/哈希|桶计数优化解法] 1002. 查找常用字符(easy) AC 2020-10-14 https://leetcode-cn.com/problems/find-common-characters/

--- src/htc/leetcode/everyday/_1002_查找常用字符.java
+++ src/htc/leetcode/everyday/_1002_查找常用字符.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 /**
  * AC
+ * 推荐桶计数解法
  * https://leetcode-cn.com/problems/find-common-characters/
  */
+
 public class _1002_查找常用字符 {
     public static void main(String[] args) {
         _1002_查找常用字符 test = new _1002_查找常用字符();
@@ -13,13 +15,48 @@ public class _1002_查找常用字符 {
         System.out.println(test.commonChars(new String[]{"cool", "lock", "cook"}));
         System.out.println(test.commonChars(new String[]{"abc", "def", "ghijk"}));
         System.out.println(test.commonChars(new String[]{"abc"}));
-        System.out.println(test.commonChars(new String[]{}));
     }
 
+    /**
+     * 桶计数：
+     * 一维数组下标代表字母，0-a 1-b ... 25-z
+     * 二维下标代表对应字母出现了多少次
+     * 如：["bella","label","roller"]最后计数对应为
+     * [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+     * [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+     * [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0]
+     * <p>
+     * 最后从上往下纵向对比取最小值min，然后把对应字母重复min次插入List中
+     * 比如上例下标0从上往下取值分别是[1,1,0],最小值为0，说明a最终出现0次，
+     * 同理，下标4(对应e)分别为[1,1,1],最小值为1,e最终出现1次
+     * 同理，下标11(l)出现2次
+     * 其他均为0次
+     * 结果为 e,l,l
+     */
     public List<String> commonChars(String[] A) {
-        if (A == null || A.length == 0) {
-            return new ArrayList<>();
+        int[][] map = new int[A.length][26];
+        for (int i = 0, len = A.length; i < len; i++) {
+            for (char c : A[i].toCharArray()) {
+                map[i][c - 'a'] = map[i][c - 'a'] + 1;
+            }
         }
+        List<String> ans = new ArrayList<>(26);
+        for (int i = 0; i < 26; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < A.length; j++) {
+                min = Math.min(map[j][i], min);
+            }
+            for (int k = 0; k < min; k++) {
+                ans.add(String.valueOf((char) ('a' + i)));
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 哈希计数，耗内存耗时
+     */
+    public List<String> commonChars1(String[] A) {
         if (A.length == 1) {
             return Arrays.asList(A);
         }
