0d693a2da3621b3f2123b1a7385b0773aa774e91
hteason <617430866@qq.com>
2020-11-14 14:56:40 +0800

    LeetCode[每日一题/排序|数组] 1122.数组的相对排序(easy) AC 2020-11-14 https://leetcode-cn.com/problems/relative-sort-array/

--- src/htc/leetcode/everyday/_2020/_14_1122_数组的相对排序.java
+++ src/htc/leetcode/everyday/_2020/_14_1122_数组的相对排序.java
@@ -13,7 +13,7 @@ public class _14_1122_数组的相对排序 {
         _14_1122_数组的相对排序 test = new _14_1122_数组的相对排序();
         System.out.println(Arrays.toString(test.relativeSortArray(
                 new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19},
-                new int[]{2, 1, 4, 3, 9, 6,7,19}
+                new int[]{2, 1, 4, 3, 9, 6}
         )));
 
     }
@@ -21,7 +21,7 @@ public class _14_1122_数组的相对排序 {
     /**
      * TreeMap统计
      */
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+    public int[] relativeSortArray1(int[] arr1, int[] arr2) {
         if (arr1 == null || arr1.length == 0) {
             return arr1;
         }
@@ -43,6 +43,35 @@ public class _14_1122_数组的相对排序 {
             int a = e.getKey();
             for (int j = 0; j < count; j++) {
                 ans[i++] = a;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 元素大小有上下限，可以考虑桶计数排序(推荐)
+     */
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr1.length == 0) {
+            return arr1;
+        }
+        int[] ans = new int[arr1.length];
+        int[] map = new int[1001];
+        for (int a1 : arr1) {
+            map[a1] = map[a1] + 1;
+        }
+        int i = 0;
+        for (int a2 : arr2) {
+            int count = map[a2];
+            while (count-- > 0) {
+                ans[i++] = a2;
+            }
+            map[a2] = 0;
+        }
+        for (int j = 0; j < 1001; j++) {
+            int count = map[j];
+            while (count-- > 0) {
+                ans[i++] = j;
             }
         }
         return ans;
