cf8155b1aa2a8c57c4a7eff9c21b8e2f30c12440
hteason <617430866@qq.com>
2020-11-18 22:07:58 +0800

    LeetCode[每日一题/贪心|环式遍历] 134.加油站(medium) AC 2020-11-18 https://leetcode-cn.com/problems/gas-station/

---
+++ src/htc/leetcode/everyday/_2020/_11/_134_加油站.java
@@ -0,0 +1,37 @@
+package htc.leetcode.everyday._2020._11;
+
+/**
+ * AC
+ * 用时过长，可用贪心优化
+ * https://leetcode-cn.com/problems/gas-station/
+ */
+public class _134_加油站 {
+    public static void main(String[] args) {
+        _134_加油站 test = new _134_加油站();
+        System.out.println(test.canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));
+        System.out.println(test.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3}));
+        System.out.println(test.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 2, 3}));
+    }
+
+    //环式遍历解法
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length;
+        for (int i = 0; i < gas.length; i++) {
+            if (gas[i] >= cost[i]) {
+                int total = gas[i] - cost[i];
+                int j = 0;
+                for (int k = (i + 1) % len; j < gas.length; j++, k = (++k) % len) {
+                    total += gas[k];
+                    if (total < cost[k]) {
+                        break;
+                    }
+                    total -= cost[k];
+                }
+                if (j == len) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
