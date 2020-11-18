1b561922b5509875e70199957d552e309b40cd3f
hteason <617430866@qq.com>
2020-11-18 22:08:25 +0800

    LeetCode[每日一题/贪心|环式遍历] 134.加油站(medium) AC 2020-11-18 https://leetcode-cn.com/problems/gas-station/

--- src/htc/leetcode/everyday/_2020/_11/_18_134_加油站.java
+++ src/htc/leetcode/everyday/_2020/_11/_18_134_加油站.java
@@ -5,9 +5,9 @@ package htc.leetcode.everyday._2020._11;
  * 用时过长，可用贪心优化
  * https://leetcode-cn.com/problems/gas-station/
  */
-public class _134_加油站 {
+public class _18_134_加油站 {
     public static void main(String[] args) {
-        _134_加油站 test = new _134_加油站();
+        _18_134_加油站 test = new _18_134_加油站();
         System.out.println(test.canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));
         System.out.println(test.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3}));
         System.out.println(test.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 2, 3}));
