38f9f51d3b1b2c30542fd393d5eace459a499ea2
hteason <617430866@qq.com>
2020-11-19 22:58:54 +0800

    LeetCode[每日一题/数组|双指针] 283.移动零(easy) AC 2020-11-19 https://leetcode-cn.com/problems/move-zeroes/

--- src/htc/leetcode/everyday/_2020/_11/_19_283_移动零.java
+++ src/htc/leetcode/everyday/_2020/_11/_19_283_移动零.java
@@ -18,9 +18,15 @@ public class _19_283_移动零 {
         arr = new int[]{1, 3, 12, 1, 4, 0, 3, 5};
         test.moveZeroes(arr);
         System.out.println(Arrays.toString(arr));
+        arr = new int[]{12, 4, 5, 0, 5, 7, 0, 7, 87, 4, 67, 9, 0, 45, 6, 900, 0, 0, 0, 0, 2};
+        test.moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public void moveZeroes(int[] nums) {
+    /**
+     * 存在重复遍历,8ms
+     */
+    public void moveZeroes0(int[] nums) {
         if (nums.length == 0 || nums.length == 1) {
             return;
         }
@@ -33,6 +39,57 @@ public class _19_283_移动零 {
                         nums[k] = tmp;
                         break;
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * 优化1
+     * 减少遍历次数,1ms
+     */
+    public void moveZeroes1(int[] nums) {
+        for (int i = 0, firstZero = -1; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                if (firstZero == -1) {
+                    firstZero = i;
+                }
+                while (++i < nums.length) {
+                    if (nums[i] != 0) {
+                        nums[firstZero] = nums[i];
+                        nums[i] = 0;
+                        firstZero++;
+                        i--;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 优化2
+     * 减少遍历次数和判断第一个0的次数,0ms,击败100%
+     */
+    public void moveZeroes(int[] nums) {
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                break;
+            }
+        }
+        int firstZero = i;
+        for (; i < nums.length; i++) {
+            if (nums[i++] == 0) {
+                while (i < nums.length) {
+                    if (nums[i] != 0) {
+                        nums[firstZero] = nums[i];
+                        nums[i] = 0;
+                        firstZero++;
+                        i--;
+                        break;
+                    }
+                    i++;
                 }
             }
         }
