1f889a3d2f965237598d18a20e02865c8b66d584
hteason <617430866@qq.com>
2020-11-25 23:00:37 +0800

    LeetCode[每日一题/排序|字符串|桶计数] 1370.上升下降字符串(easy) AC 2020-11-25 https://leetcode-cn.com/problems/increasing-decreasing-string/

---
+++ src/htc/leetcode/everyday/_2020/_11/_25_1370_上升下降字符串.java
@@ -0,0 +1,49 @@
+package htc.leetcode.everyday._2020._11;
+
+/**
+ * AC
+ * Í°¼ÆÊý
+ * https://leetcode-cn.com/problems/increasing-decreasing-string/
+ *
+ * @date 2020/11/25
+ */
+public class _25_1370_ÉÏÉýÏÂ½µ×Ö·û´® {
+    public static void main(String[] args) {
+        _25_1370_ÉÏÉýÏÂ½µ×Ö·û´® test = new _25_1370_ÉÏÉýÏÂ½µ×Ö·û´®();
+        System.out.println(test.sortString("aaaabbbbcccc"));
+        System.out.println(test.sortString("rat"));
+        System.out.println(test.sortString("leetcode"));
+        System.out.println(test.sortString("ggggggg"));
+        System.out.println(test.sortString("spo"));
+        System.out.println(test.sortString(""));
+    }
+
+    public String sortString(String s) {
+        int[] map = new int[26];
+        int len = 0;
+        for (char c : s.toCharArray()) {
+            map[c - 'a']++;
+            len++;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (len > 0) {
+            int i = 0;
+            for (; i < 26; i++) {
+                if (map[i] != 0) {
+                    sb.append((char) (i + 'a'));
+                    len--;
+                    map[i]--;
+                }
+            }
+            for (i--; i >= 0; i--) {
+                if (map[i] != 0) {
+                    sb.append((char) (i + 'a'));
+                    len--;
+                    map[i]--;
+                }
+            }
+
+        }
+        return sb.toString();
+    }
+}
