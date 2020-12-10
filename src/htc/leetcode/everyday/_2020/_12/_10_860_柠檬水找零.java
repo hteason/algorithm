3976c35b37f06b04fbdb47754ada3dec444e3140
hteason <617430866@qq.com>
2020-12-10 23:55:33 +0800

    LeetCode[每日一题/贪心|dp] 860.柠檬水找零(easy) AC 2020-12-10 https://leetcode-cn.com/problems/lemonade-change/

--- src/htc/leetcode/everyday/_2020/_12/_10_860_柠檬水找零.java
+++ src/htc/leetcode/everyday/_2020/_12/_10_860_柠檬水找零.java
@@ -55,4 +55,41 @@ public class _10_860_ÄûÃÊË®ÕÒÁã {
         }
         return true;
     }
+
+    //ÓÅ»¯1
+    public boolean lemonadeChange2(int[] bills) {
+        if (bills == null || bills.length == 0 || bills[0] == 10 || bills[0] == 20) {
+            return false;
+        }
+        int five = 0;
+        int ten = 0;
+        for (int bill : bills) {
+            switch (bill) {
+                case 5:
+                    five++;
+                    break;
+
+                case 10:
+                    if (five <= 0) {
+                        return false;
+                    }
+                    five--;
+                    ten++;
+                    break;
+
+                case 20:
+                    if (ten >= 1 && five >= 1) {
+                        ten--;
+                        five--;
+                    } else if (ten == 0 && five >= 3) {
+                        five -= 3;
+                    } else {
+                        return false;
+                    }
+                    break;
+                default:
+            }
+        }
+        return true;
+    }
 }
